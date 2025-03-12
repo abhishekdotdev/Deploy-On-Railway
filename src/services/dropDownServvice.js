81352c419ad927707f6b80c1a15ac23e585a43c6
abhishekkumar085 <abhishek950820@gmail.com>
2025-03-12 12:25:37 +0530

    Added count of the doc

--- src/services/dropDownServvice.js
+++ src/services/dropDownServvice.js
@@ -3,8 +3,11 @@ const dropDownItem = require('../schema/userDropdown');
 async function dropDown() {
   try {
     const response = await dropDownItem.find();
-    const countDoc = await dropDownItem.aggregate([{ $count: 'myCount' }]);
-    return { response, countDoc };
+    // const countDoc = await dropDownItem.aggregate([{ $count: 'myCount' }]);
+    const data = response.slice(0, 5);
+    const countDoc = data.length;
+
+    return { data, countDoc };
   } catch (error) {
     console.log(error);
     throw error;
