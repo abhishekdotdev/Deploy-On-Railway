078950e22521d58a5f9e48f0871cee6feff77cb2
shubham khelkar <shubhamkhelkar987@gmail.com>
2025-03-26 17:17:04 +0530

    adddthe

--- src/controllers/dropdown.js
+++ src/controllers/dropdown.js
@@ -8,6 +8,27 @@ async function dropDown(req, res) {
       message: 'dropdown getting successfully!',
       data: response,
       error: {},
+      
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'something went wrong!',
+      data: {},
+      error: error,
+    });
+  }
+}
+async function dropDowns(req, res) {
+  try {
+    const response = await DropdownService.dropDown();
+    res.status(200).json({
+      success: true,
+      message: 'dropdown getting successfully!',
+      data: response,
+      error: {},
+
     });
   } catch (error) {
     console.log(error);
